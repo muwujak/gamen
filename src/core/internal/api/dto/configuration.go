5df54d63f8ed5557ab9c9062fde0504ed8a1a48b
mujak27 <mujakoverflow@gmail.com>
2025-08-05 16:47:36 +0000

    chore: dto configuration

---
+++ src/core/internal/api/dto/configuration.go
@@ -0,0 +1,21 @@
+package dto
+
+import (
+	"github.com/google/uuid"
+	"github.com/mujak27/gamen/src/core/internal/models"
+)
+
+type ConfigurationCreatePayload struct {
+	Name                string      `json:"name" validate:"required"`
+	Description         string      `json:"description" validate:"required"`
+	ConfigurationTypeID uuid.UUID   `json:"configuration_type_id" validate:"required"`
+	TeamID              uuid.UUID   `json:"team_id" validate:"required"`
+	Data                models.JSON `json:"data" validate:"required"`
+}
+
+type ConfigurationUpdatePayload struct {
+	ID          uuid.UUID   `json:"id" validate:"required"`
+	Name        string      `json:"name" validate:"required"`
+	Description string      `json:"description" validate:"required"`
+	Data        models.JSON `json:"data" validate:"required"`
+}
